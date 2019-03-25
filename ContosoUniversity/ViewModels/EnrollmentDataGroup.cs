d93ffc2347d7e7842bad1b4f0290e17c11620be6
irinejs <irinejs@gmail.com>
2019-03-25 15:30:25 +0100

    ViewModel EnrollmentDataGroup created

---
+++ ContosoUniversity/ViewModels/EnrollmentDataGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class EnrollmentDataGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentGroup { get; set; }
+    }
+}
