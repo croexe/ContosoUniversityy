13494ca1ab1ce3a7752557a2ecd35a1a736e3d73
irinejs <irinejs@gmail.com>
2019-03-29 17:01:38 +0100

    CourseController Modified Index Method

--- ContosoUniversity/Controllers/CourseController.cs
+++ ContosoUniversity/Controllers/CourseController.cs
@@ -17,10 +17,30 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Course
-        public ActionResult Index()
+        public ActionResult Index(int? SelectedDepartment)
         {
+
+            var departments = db.Departments.OrderBy(q => q.Name).ToList();
+            ViewBag.SelectedDepartment = new SelectList(departments, "DepartmentID", "Name", SelectedDepartment);
+            int departmentID = SelectedDepartment.GetValueOrDefault();
+
+            IQueryable<Course> courses = db.Courses
+                .Where(c => !SelectedDepartment.HasValue || c.DepartmentID == departmentID)
+                .OrderBy(d => d.CourseID)
+                .Include(d => d.Department);
+            var sql = courses.ToString();
+            return View(courses.ToList());
+
+            /*
             var courses = db.Courses.Include(c => c.Department);
             return View(courses.ToList());
+            */
+
+            /*
+            var courses = db.Courses;
+            var sql = courses.ToString();
+            return View(courses.ToList());
+            */
         }
 
         // GET: Course/Details/5
